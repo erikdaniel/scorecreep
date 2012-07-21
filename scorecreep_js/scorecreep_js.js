9c7adc27ee87fd7b2a8c8ca19092842103045d8c
Erik Daniel <erikwdaniel@gmail.com>
2012-07-20 22:41:27 -0400

    Added js module

---
+++ scorecreep_js/scorecreep_js.js
@@ -0,0 +1,22 @@
+/**
+ * Scorecreep Javascript
+ */
+jQuery(document).ready(function($) {
+  var $sel = $('#edit-field-performance-team-und'),
+    $perfList = $('#edit-field-performance-performer-und'),
+    $performersOrig = $('#edit-field-performance-performer-und option').clone();
+  $('#edit-field-performance-performer-und option').remove();
+  $sel.change(function (e) {
+    var selected = $(this).find('option:selected').text(),
+      current = [],
+      $performers = $performersOrig.clone();
+    $('#edit-field-performance-performer-und option').remove();
+    $perfList.append($performers);
+    $perfList.find('option').each(function (index, item) {
+      if (index > 0 && $(item).text().indexOf(selected) === -1) {
+        $(item).remove();
+      }
+    });
+
+  });
+});
